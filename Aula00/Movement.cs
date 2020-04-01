66093e3bbe09bfbe20d677f17c0d3bc2dca5925a
HikkusT <luizhenrique98@gmail.com>
2020-03-31 22:53:35 -0300

    First commit

---
+++ Aula00/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Recebe um gameobject que vai atrás pelo inspetor
+    public GameObject target;
+    
+    // Componentes da velocidade
+    Vector3 dir;
+    public float speed = 2;
+
+    void Start()
+    {
+        // Pega referência do componente de MeshRenderer e troca a cor
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // A cada frame calcula a nova direção que deve andar pra chegar no gameobject target
+        dir = target.transform.position - transform.position;
+
+        // Calcula a velocidade
+        Vector3 velocity = speed * dir.normalized;
+
+        // Anda na direção o equivalente a Time.deltaTime, que é o tempo de duração do frame
+        transform.position = transform.position + velocity * Time.deltaTime;
+    }
+}
---
+++ Aula00/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Passa um prefab pelo inspetor
+    public GameObject gameObject;
+
+    void Start()
+    {
+        // Cria um objeto na scene a partir desse prefab
+        Instantiate(gameObject);
+    }
+
+    void Update()
+    {
+        Instantiate(gameObject);
+    }
+}
