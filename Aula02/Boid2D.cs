1c2f272fe7c3bf7df24ec265813206bf1b837e72
luiz-alves-playkids <luiz.alves@playkids.com>
2020-04-21 13:48:08 -0300

    Add aula 2

---
+++ Aula02/Boid2D.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid2D : MonoBehaviour
+{
+    public Vector2 velocity;
+    public Vector2 acceleration;
+    public float maxSpeed;
+    public Vector2 target;
+    public List<Boid2D> neighbors;
+    public float viewRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Field of View simples. Calcula a distância e vê se é maior do que um raio definido para o objeto
+    public bool IsInsideFieldOfView(Vector2 point)
+    {
+        float distance = (point - (Vector2)transform.position).magnitude;
+        return distance <= viewRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug de vizinhos
+        foreach (Boid2D neighbor in neighbors)
+            Debug.DrawLine(transform.position, neighbor.transform.position, Color.magenta);
+
+        // Removendo a Demo de seguir o mouse
+        //acceleration = Vector2.zero;
+        //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 desiredVelocity = target - (Vector2)transform.position;
+        //SteerTowards(desiredVelocity);
+
+        // Atualizando os vetores
+        velocity = velocity + acceleration * Time.deltaTime;
+        velocity = Vector2.ClampMagnitude(velocity, maxSpeed); // Travando numa velocidade máxima
+        transform.position = transform.position + (Vector3)velocity * Time.deltaTime;
+
+        // Alinhando a rotação
+        transform.right = velocity;
+
+        // Debug para visualizar os vetores
+        Debug.DrawRay(transform.position, velocity, Color.red);
+        Debug.DrawRay(transform.position + (Vector3)velocity, acceleration, Color.yellow);
+    }
+
+    void SteerTowards(Vector2 desiredVelocity)
+    {
+        acceleration += desiredVelocity - velocity;
+    }
+}
---
+++ Aula02/World.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public Boid2D prefab;
+    public int numBoids;
+    public List<Boid2D> boids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn boids
+        for (int i = 0; i < numBoids; i++)
+        {
+            Boid2D boid = Instantiate(prefab, Random.insideUnitCircle * 3, Quaternion.identity);
+            boids.Add(boid);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // A cada frame temos que lembrar de atualizar os vizinhos de cada boid
+        foreach (Boid2D boid in boids)
+            UpdateNeighbors(boid);
+    }
+
+    void UpdateNeighbors(Boid2D subject)
+    {
+        List<Boid2D> updateNeighbors = new List<Boid2D>();
+
+        // Para cada boid do mundo(caso não seja ele mesmo) perguntamos ao boid em questão se é vizinho 
+        foreach (Boid2D boid in boids)
+            if (boid != subject && subject.IsInsideFieldOfView(boid.transform.position))
+                updateNeighbors.Add(boid);
+        subject.neighbors = updateNeighbors;
+    }
+}
