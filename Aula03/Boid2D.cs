146cbf9de22f37d8cb215c7559d71a6b895696c8
luiz-alves-playkids <luiz.alves@playkids.com>
2020-04-25 21:22:09 -0300

    Add third class

---
+++ Aula03/Boid2D.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid2D : MonoBehaviour
+{
+    public Vector2 velocity;
+    public Vector2 acceleration;
+    public float maxSpeed;
+    public float minSpeed;
+    public Vector2 target;
+    public List<Boid2D> neighbors;
+    public float viewRadius;
+    public float viewAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Random.insideUnitCircle.normalized * maxSpeed;
+    }
+
+    // Field of View simples. Calcula a distância e vê se é maior do que um raio definido para o objeto
+    public bool IsInsideFieldOfView(Vector2 point)
+    {
+        float distance = (point - (Vector2)transform.position).magnitude;
+        return distance <= viewRadius && Vector2.Angle((point - (Vector2)transform.position), velocity) <= viewAngle / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Vector2.zero;
+        // Debug de vizinhos
+        foreach (Boid2D neighbor in neighbors)
+            Debug.DrawLine(transform.position, neighbor.transform.position, Color.magenta);
+
+        if (neighbors.Count > 0)
+        {
+            Align();
+            Cohesion();
+            Separation();
+        }
+
+        // Atualizando os vetores
+        velocity = velocity + acceleration * Time.deltaTime;
+        velocity = Vector2.ClampMagnitude(velocity, maxSpeed); // Travando numa velocidade máxima
+        if (velocity.magnitude < minSpeed)
+            velocity = velocity.normalized * minSpeed;
+        transform.position = transform.position + (Vector3)velocity * Time.deltaTime;
+
+        // Alinhando a rotação
+        transform.right = velocity;
+
+        // Debug para visualizar os vetores
+        Debug.DrawRay(transform.position, velocity, Color.red);
+        Debug.DrawRay(transform.position + (Vector3)velocity, acceleration, Color.yellow);
+    }
+
+    void SteerTowards(Vector2 desiredVelocity, float intensity)
+    {
+        acceleration += (desiredVelocity.normalized * maxSpeed - velocity) * intensity;
+    }
+
+    void Align()
+    {
+        Vector2 sumVelocities = Vector2.zero;
+
+        foreach (Boid2D neighbor in neighbors)
+        {
+            sumVelocities += neighbor.velocity;
+        }
+
+        sumVelocities /= neighbors.Count;
+        SteerTowards(sumVelocities, 1);
+    }
+
+    void Cohesion()
+    {
+        Vector2 sumPositions = Vector2.zero;
+
+        foreach (Boid2D neighbor in neighbors)
+        {
+            sumPositions += (Vector2)neighbor.transform.position;
+        }
+
+        Vector2 center = sumPositions / neighbors.Count;
+        Vector2 desiredVelocity = center - (Vector2)transform.position;
+        SteerTowards(desiredVelocity, 2);
+    }
+
+    void Separation()
+    {
+        Vector2 sumSeparations = Vector2.zero;
+
+        foreach (Boid2D neighbor in neighbors)
+        {
+            Vector2 separation = transform.position - neighbor.transform.position;
+            sumSeparations += separation / separation.sqrMagnitude;
+        }
+
+        Vector2 desiredVelocity = sumSeparations / neighbors.Count;
+        SteerTowards(desiredVelocity, 2);
+    }
+}
---
+++ Aula03/World.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public Boid2D prefab;
+    public int numBoids;
+    public List<Boid2D> boids;
+    private Vector2 bl, tr, worldDimensions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bl = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, -10));
+        tr = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, -10));
+        worldDimensions = new Vector2(tr.x - bl.x, tr.y - bl.y);
+
+        for (int i = 0; i < numBoids; i++)
+        {
+            Boid2D boid = Instantiate(prefab, Random.insideUnitCircle * 3, Quaternion.identity);
+            boids.Add(boid);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Boid2D boid in boids)
+            FixPosition(boid);
+
+        // A cada frame temos que lembrar de atualizar os vizinhos de cada boid
+        foreach (Boid2D boid in boids)
+            UpdateNeighbors(boid);
+    }
+
+    // Faz dar a volta no mapa
+    void FixPosition(Boid2D subject)
+    {
+        float boundedX = Mathf.Repeat(subject.transform.position.x - bl.x, worldDimensions.x) + bl.x;
+        float boundedY = Mathf.Repeat(subject.transform.position.y - bl.y, worldDimensions.y) + bl.y;
+        subject.transform.position = new Vector2(boundedX, boundedY);
+    }
+
+    void UpdateNeighbors(Boid2D subject)
+    {
+        List<Boid2D> updateNeighbors = new List<Boid2D>();
+
+        // Para cada boid do mundo(caso não seja ele mesmo) perguntamos ao boid em questão se é vizinho 
+        foreach (Boid2D boid in boids)
+            if (boid != subject && subject.IsInsideFieldOfView(boid.transform.position))
+                updateNeighbors.Add(boid);
+        subject.neighbors = updateNeighbors;
+    }
+}
